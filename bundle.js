d7d3799d8fa63da9bed3ed9f9efc268780bee5ff
whamer100 <whammer100@aol.com>
2019-07-21 22:30:26 -0700

    i mean it works

---
+++ bundle.js
@@ -0,0 +1,20 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function (global){
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ex = __importStar((typeof window !== "undefined" ? window['ex'] : typeof global !== "undefined" ? global['ex'] : null));
+const game = new ex.Engine({
+    width: 800,
+    height: 600,
+});
+game.start();
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}]},{},[1]);
---
+++ index.ts
@@ -0,0 +1,8 @@
+import * as ex from "excalibur"
+
+const game = new ex.Engine({
+  width: 800,
+  height: 600,
+})
+
+game.start()
